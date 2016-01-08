d57171d3d1ee9cf186d6aff0a3f24b9937c8dc83
test bayteq <testbayteq@gmail.com>
2016-01-08 15:25:29 -0500

    Initial Commit

---
+++ Attributor/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Attributor
+//
+//  Created by Bayteq on 8/1/16.
+//  Copyright © 2016 Bayteq CiaLtda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Attributor/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Attributor
+//
+//  Created by Bayteq on 8/1/16.
+//  Copyright © 2016 Bayteq CiaLtda. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
